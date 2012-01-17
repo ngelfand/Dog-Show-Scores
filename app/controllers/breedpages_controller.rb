9125154e81cc1e2b99fae2d9c110b6affa39b3ec
Natasha Gelfand <ngelfand@gmail.com>
2012-01-17 13:36:10 -0800

    Collie page tweaks

--- app/controllers/breedpages_controller.rb
+++ app/controllers/breedpages_controller.rb
@@ -7,9 +7,10 @@ class BreedpagesController < ApplicationController
       @start_date = Date.civil(params[:start_date][:year].to_i, params[:start_date][:month].to_i, params[:start_date][:day].to_i)
       @end_date = DateTime.civil(params[:end_date][:year].to_i, params[:end_date][:month].to_i, params[:end_date][:day].to_i)
       @scores = Obedscore.find(:all, :joins => "obedscores inner join shows as s on obedscores.show_id = s.id inner join dogs as d on obedscores.dog_id = d.id",
-      :conditions=>["s.date >= ? AND s.date <= ? AND (lower(d.breed) = ? OR lower(d.breed) = ? OR lower(d.breed) = ?)", 
-        @start_date, @end_date, "rough collie", "smooth collie", "collie"],
-      :order=>'s.date ASC')   
+      :conditions=>["s.date >= ? AND s.date <= ? AND (lower(d.breed) = ? OR lower(d.breed) = ? OR lower(d.breed) = ?) AND
+        (obedscores.classname LIKE ? OR obedscores.classname LIKE ? OR obedscores.classname LIKE ?)", 
+        @start_date, @end_date, "rough collie", "smooth collie", "collie", "Novice%", "Open%", "Utility%"],
+      :order=>'d.akc_name ASC')   
       
       # this is very very very bad but unfortunately I don't have a good way to do it, I need to relate obedscore to obedclass somehow to make 
       # it more efficient
